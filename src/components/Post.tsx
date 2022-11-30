cb73cee7a4283003581e1b1b94d7f916008b714d
Shan15Dev <shansai@bluewin.ch>
2022-11-30 19:11:42 +0100

    post.tsx has form with deletion when submitted

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,38 +1,64 @@
-import React, {ChangeEvent, SyntheticEvent, useState} from 'react';
+import React, {ChangeEvent, SyntheticEvent, useEffect, useState} from 'react';
 
 type PostRequest = () => {
-    title : string,
-    message : string,
-    nickname : string
+    title: string,
+    message: string,
+    nickname: string
 }
 
-
+const defaultModel: any = {
+    title: "",
+    message: "",
+    nickname: ""
+}
 
 function Post() {
-    const [postRequest, setPostRequest] = useState<PostRequest>();
-    const handleSubmit = (event : SyntheticEvent) => {
+    const [postRequest, setPostRequest] = useState<any>(defaultModel);
+    const [allPosts, setAllPosts] = useState<PostRequest[]>(defaultModel);
+    const posts: PostRequest[] = []
+
+    const handleSubmit = (event: SyntheticEvent) => {
+        posts.push(postRequest)
+        setPostRequest(defaultModel)
+        setAllPosts(posts)
+        console.log(posts)
 
     }
 
-    const handleChange = (event : ChangeEvent<HTMLInputElement>) => {
-        setPostRequest({
-            ...postRequest,
-            [event.target.name] : event.target.value,
-        })
+    const handleChange = (event: any) => {
+        if (event.target.name == "nickname" && event.target.value == "Nickname wählen") return;
+        setPostRequest({...postRequest, [event.target.name]: event.target.value})
+        console.log(postRequest)
     }
+
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input
-                        type="text"
-                        value={postRequest?.name}
-                        onChange={(e) => handleChange(e)}
-                    />
-                </label>
-            </form>
+            <label>
+                Title:
+                <input
+                    type="text"
+                    name="title"
+                    value={postRequest?.title}
+                    onChange={(e) => handleChange(e)}
+                />
+            </label>
+            <label>
+                Message:
+                <textarea
+                    name="message"
+                    value={postRequest?.message}
+                    onChange={(e) => handleChange(e)}
+                />
+            </label>
+            <label>Author</label>
+            <select id="nickname" name="nickname" onChange={handleChange} required>
+                <option defaultValue="INVALID">Nickname wählen</option>
+                <option value="Foo">Foo</option>
+                <option value="Bar">Bar</option>
+                <option value="Shan">Shan</option>
+            </select>
+            <button onClick={handleSubmit}>submit</button>
         </div>
     );
 }
